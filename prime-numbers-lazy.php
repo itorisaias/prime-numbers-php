e4b7ce1f6eef330007eca0b81e31256c70b3b2a9
Itor Isaias da Silva <itor.isaias@gmail.com>
2022-08-12 09:49:05 -0300

    Create prime-numbers-lazy.php
    find prime without perfomance

---
+++ prime-numbers-lazy.php
@@ -0,0 +1,34 @@
+<?php
+
+// A prime number (or a prime) is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers. For example, 5 is prime because the only ways of writing it as a product, 1 × 5 or 5 × 1, involve 5 itself. Please write a program that will list the prime numbers up to 100.
+
+$expectedResults = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97];
+
+function isPrime ($number) {
+  if ($number <= 1) {
+    return false;
+  }
+  
+  for ($i=2; $i < $number; $i++) { 
+    $remainder = $number % $i;
+
+    if ($remainder == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+$arrayResult = [];
+
+for ($i = 0; $i <= 100; $i++) {
+
+  $result = isPrime($i);
+
+  if ($result) {
+    array_push($arrayResult, $i);
+  }
+}
+
+var_dump($expectedResults == $arrayResult);
